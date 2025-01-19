4cc6ff30ade67a667865ee1381ba39c582e703ea
RebecaSamiadev <168788291+RebecaSamiadev@users.noreply.github.com>
2025-01-18 23:42:21 -0300

    Add files via upload

---
+++ testLed.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+const uint led_pin_red = 12;
+int main(){
+uint a = 100;
+
+gpio_init(led_pin_red);
+gpio_set_dir(led_pin_red, GPIO_OUT);
+stdio_init_all();
+while (true){
+a++;
+if (a % 2)
+printf("Blinking!\r\n");
+gpio_put(led_pin_red, true);
+sleeps_ms(1000);
+gpio_put(led_pin_red, falar);
+sleeps_ms(1000);
+}
+}
